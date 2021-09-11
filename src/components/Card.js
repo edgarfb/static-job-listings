634c6c661bc51b569cf41b5788d597779c98afc8
Edgar Britez <edgarfbritez@gmail.com>
2021-09-11 13:23:52 -0300

    project finished

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,28 +9,30 @@ function Card(props) {
       <div className={styles.logo}>
         <img src={props.logo} alt={`${props.company} Logo`} />
       </div>
-      <aside className={styles.mainTitle}>
-        <h3>
-          {props.name}
-          {props.new && <span className={styles.titleTag}>New!</span>}
-          {props.featured && <span className={styles.titleTag}>FEATURED</span>}
-        </h3>
-        <h2>{props.position}</h2>
-        <div className={styles.moreInfo}>
-          <div className={styles.moreInfo__inner}>
-            <span>{props.postedAt}</span>
-            <span className={styles.dot}>.</span>
-            <span>{props.contract}</span>
-            <span className={styles.dot}>.</span>
-            <span>{props.location}</span>
+      <div className={styles.deskContainer}>
+        <aside className={styles.mainTitle}>
+          <h3>
+            {props.name}
+            {props.new && <span className={styles.titleTag}>New!</span>}
+            {props.featured && <span className={styles.titleTag}>FEATURED</span>}
+          </h3>
+          <h2>{props.position}</h2>
+          <div className={styles.moreInfo}>
+            <div className={styles.moreInfo__inner}>
+              <span>{props.postedAt}</span>
+              <span className={styles.dot}>.</span>
+              <span>{props.contract}</span>
+              <span className={styles.dot}>.</span>
+              <span>{props.location}</span>
+            </div>
           </div>
-        </div>
-      </aside>
+        </aside>
 
-      <div className={styles.tags}>
-        {props.languages.map((lang) => {
-          return <Button onClick={() => props.onClickTag(lang)}>{lang}</Button>;
-        })}
+        <div className={styles.tags}>
+          {props.languages.map((lang) => {
+            return <Button onClick={() => props.onClickTag(lang)}>{lang}</Button>;
+          })}
+        </div>
       </div>
     </div>
   );
